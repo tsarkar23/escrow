aefb886d537105f8f49082496881d21f3572a9b9
tsarkar23 <tuhin91@gmail.com>
2021-10-12 18:16:54 -0500

    clients

--- client/setup.py
+++ client/setup.py
@@ -11,22 +11,25 @@ from solana.system_program import CreateAccountWithSeedParams, create_account_wi
 from solana.transaction import Transaction, AccountMeta, TransactionInstruction
 from solana.system_program import SYS_PROGRAM_ID
 from spl.token.client import Token
-from spl.token.constants import TOKEN_PROGRAM_ID
 from solana import keypair
 from solana.publickey import PublicKey
 from solana.sysvar import SYSVAR_RENT_PUBKEY
+from struct import *
+import struct
 
-# Payer account
-http_client = Client("https://api.devnet.solana.com")
-first_key_in_key_folder = glob.glob("../keys/M*")[0]
-payer_loaded_account = solana.keypair.Keypair(json.load(open(first_key_in_key_folder))[:32])
+
+TOKEN_PROGRAM_ID: PublicKey = PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
+
+
+payer_loaded_account = solana.keypair.Keypair.generate()
 payer_public_key = payer_loaded_account.public_key
 
-# Create alice account
+http_client = Client("https://api.devnet.solana.com")
+
+# todo: change program_id to system program, change data size =0, change lampord for both alice and bob
 alice_keypair = solana.keypair.Keypair.generate()
 alice_pubkey = alice_keypair.public_key
 
-# Create bob account
 bob_keypair = solana.keypair.Keypair.generate()
 bob_pubkey = bob_keypair.public_key
 
@@ -34,24 +37,133 @@ print(f"Payer Account: {payer_public_key}")
 print(f"alice Account: {alice_pubkey}")
 print(f"bob Account: {bob_pubkey}")
 
-http_client.request_airdrop(payer_public_key, 1000000000)
-time.sleep(20)
+import time
+http_client.request_airdrop(payer_loaded_account.public_key, 1000000000)
+time.sleep(15)
 http_client.request_airdrop(alice_pubkey, 1000000000)
-time.sleep(20)
+time.sleep(15)
 http_client.request_airdrop(bob_pubkey, 1000000000)
-# mention desgin choice between alice/bob vs payer
+print(http_client.get_balance(alice_pubkey))
 
-# Get program id
+
+X_mint_account_address = Token.create_mint(conn= http_client, payer = payer_loaded_account,\
+                                           mint_authority = payer_loaded_account.public_key,\
+                                           decimals = 0, program_id = TOKEN_PROGRAM_ID, )
+Y_mint_account_address = Token.create_mint(conn= http_client, payer = payer_loaded_account,\
+                                           mint_authority = payer_loaded_account.public_key,\
+                                           decimals = 0, program_id = TOKEN_PROGRAM_ID, )
+alice_X_token_account = X_mint_account_address.create_associated_token_account(alice_pubkey)
+alice_Y_token_account = Y_mint_account_address.create_associated_token_account(alice_pubkey)
+bob_X_token_account = X_mint_account_address.create_associated_token_account(bob_pubkey)
+bob_Y_token_account = Y_mint_account_address.create_associated_token_account(bob_pubkey)
+
+X_mint_account_address.mint_to(alice_X_token_account,payer_loaded_account,1000)
+Y_mint_account_address.mint_to(bob_Y_token_account,payer_loaded_account,100)
+
+# escrow meta-data account
 deployed_program_key = glob.glob("../dist/program/*.json")[0]
 deployed_program_key_account = solana.keypair.Keypair(json.load(open(deployed_program_key))[:32])
 program_id = deployed_program_key_account.public_key
 
-# Writing keys to file
-with open("../keys/alice_pubkey.json", 'w') as a:
-    json.dump(str(alice_pubkey), a)
+# Seed+password for sending data
+password = (20).to_bytes(8,byteorder='little')
+x_seeds = [
+    b"vault_x",
+    password,
+    bytes(alice_pubkey),
+    bytes(bob_pubkey),
+    bytes(X_mint_account_address.pubkey),
+    bytes(Y_mint_account_address.pubkey),
+]
 
-with open("../keys/bob_pubkey.json", 'w') as a:
-    json.dump(str(bob_pubkey), a)
+y_seeds = [
+    b"vault_y",
+    password,
+    bytes(alice_pubkey),
+    bytes(bob_pubkey),
+    bytes(X_mint_account_address.pubkey),
+    bytes(Y_mint_account_address.pubkey),
+]
 
-with open("../keys/program_id.json", 'w') as a:
-    json.dump(str(program_id), a)
+escrow_seeds = [
+    b"escrow",
+    password,
+    bytes(alice_pubkey),
+    bytes(bob_pubkey),
+    bytes(X_mint_account_address.pubkey),
+    bytes(Y_mint_account_address.pubkey),
+]
+
+# create vaults, necessary?
+vaultx, xseed = PublicKey.find_program_address(seeds=x_seeds,program_id=program_id)
+vaulty, yseed = PublicKey.find_program_address(seeds=y_seeds,program_id=program_id)
+escrow_address, escrow_seed = PublicKey.find_program_address(seeds=escrow_seeds,program_id=program_id)
+
+print('Start Initializing Accounts')
+# initialize transaction
+x_val = 13
+y_val = 15
+data = pack('<BQQ', 0,x_val,y_val)+password
+tx = Transaction()
+tx_instruction = TransactionInstruction(
+    keys=[AccountMeta(pubkey=escrow_address, is_signer=False, is_writable=True),
+          AccountMeta(pubkey=X_mint_account_address.pubkey, is_signer=False, is_writable=False),
+          AccountMeta(pubkey=Y_mint_account_address.pubkey, is_signer=False, is_writable=False),
+          AccountMeta(pubkey=vaultx, is_signer=False, is_writable=True),
+          AccountMeta(pubkey=vaulty, is_signer=False, is_writable=True),
+          AccountMeta(pubkey=payer_public_key, is_signer=True, is_writable=False),
+          AccountMeta(pubkey=alice_pubkey, is_signer=False, is_writable=False),
+          AccountMeta(pubkey=bob_pubkey, is_signer=False, is_writable=False),
+          AccountMeta(pubkey=TOKEN_PROGRAM_ID, is_signer=False, is_writable=False),
+          AccountMeta(pubkey=SYSVAR_RENT_PUBKEY, is_signer=False, is_writable=False),
+          AccountMeta(pubkey=SYS_PROGRAM_ID, is_signer=False, is_writable=False),
+         ],
+    program_id=program_id,
+    data=data,
+)
+
+tx = tx.add(tx_instruction)
+transaction_results = http_client.send_transaction(tx, *[payer_loaded_account])#, *[payer_loaded_account])
+print('Initializing Accounts Ended')
+
+time.sleep(30)
+
+# Alice deposit X
+print('Alice is depositing X')
+data = pack('<B', 1)
+tx = Transaction()
+tx_instruction = TransactionInstruction(
+    keys=[AccountMeta(pubkey=escrow_address, is_signer=False, is_writable=True),
+          AccountMeta(pubkey=alice_X_token_account, is_signer=False, is_writable=True), # x_a info
+          AccountMeta(pubkey=vaultx, is_signer=False, is_writable=True),
+          AccountMeta(pubkey=alice_pubkey, is_signer=True, is_writable=False),
+          AccountMeta(pubkey=TOKEN_PROGRAM_ID, is_signer=False, is_writable=False),
+         ],
+    program_id=program_id,
+    data=  data,
+)
+
+tx = tx.add(tx_instruction)
+transaction_results = http_client.send_transaction(tx, *[alice_keypair])
+
+time.sleep(30)
+
+print('Bob is depositing Y')
+# Bob deposit Y
+data = pack('<B', 1)
+tx = Transaction()
+tx_instruction = TransactionInstruction(
+    keys=[AccountMeta(pubkey=escrow_address, is_signer=False, is_writable=True),
+          AccountMeta(pubkey=bob_Y_token_account, is_signer=False, is_writable=True),
+          AccountMeta(pubkey=vaulty, is_signer=False, is_writable=True),
+          AccountMeta(pubkey=bob_pubkey, is_signer=True, is_writable=False),
+          AccountMeta(pubkey=TOKEN_PROGRAM_ID, is_signer=False, is_writable=False),
+         ],
+    program_id=program_id,
+    data=  data,
+)
+
+tx = tx.add(tx_instruction)
+
+
+transaction_results = http_client.send_transaction(tx, *[bob_keypair])
