01645abb4daa8e5417c08574fc2f234c2602144f
Mohammadhossein Shafinia <mhshafinia@Mohammadhosseins-MacBook-Pro.local>
2021-10-13 11:45:15 -0500

    removed withdraw_token and deposit_token functions and put them in the main process

--- src/processor.rs
+++ src/processor.rs
@@ -38,171 +38,6 @@ impl Processor {
         }
     }
 
-    pub fn process_withdrawal(
-        accounts: &[AccountInfo],
-        pass: u64,
-        program_id: &Pubkey) -> ProgramResult {
-        let account_info_iter = &mut accounts.iter();
-        let escrow_info = next_account_info(account_info_iter)?; // mint  public address
-        let taker_token_info = next_account_info(account_info_iter)?;
-        let vault_info = next_account_info(account_info_iter)?; // mint  public address
-        let taker_info = next_account_info(account_info_iter)?;
-        let token_program_info = next_account_info(account_info_iter)?; // token_program_id
-        msg!("process_withdrawal 1");
-        let escrow_data = EscrowData::try_from_slice(&escrow_info.data.borrow())?;
-        
-        let amount;
-        msg!("process_withdrawal 2");
-        if escrow_data.init_deposit_status <= 2 {
-            msg!("Hey it is not the time to withdraw, you may go ahead and cancel to get back your token!");
-            return Err(ProgramError::InvalidAccountData);
-        }
-        msg!("process_withdrawal 3");
-        if taker_info.key.as_ref() == escrow_data.a_pub_key.as_ref() {
-            if escrow_data.is_a_withdrawed != 0 {
-                msg!("Already done!");
-                return Err(ProgramError::InvalidAccountData);
-            }
-            amount = escrow_data.yval;
-        } else if taker_info.key.as_ref() == escrow_data.b_pub_key.as_ref() {
-            if escrow_data.is_b_withdrawed != 0 {
-                msg!("Already done!");
-                return Err(ProgramError::InvalidAccountData);
-            }
-            amount = escrow_data.xval;
-        } else{
-            msg!("Not authorized!");
-            return Err(ProgramError::InvalidAccountData);
-        };
-        msg!("process_withdrawal 4");
-        let tmp = pass.to_le_bytes();
-        let escrow_seeds = &[
-            b"escrow",
-            tmp.as_ref(),
-            escrow_data.a_pub_key.as_ref(),
-            escrow_data.b_pub_key.as_ref(),
-            escrow_data.mint_x_pub_key.as_ref(),
-            escrow_data.mint_y_pub_key.as_ref(),
-        ];
-
-        // Do the checkings
-        msg!("process_withdrawal 5");
-        withdraw_token(
-            program_id,
-            escrow_info,
-            vault_info,
-            taker_token_info,
-            token_program_info,
-            escrow_seeds,
-            amount,
-        )?;
-        msg!("process_withdrawal 6");
-        let escrow_data = EscrowData {
-            xval: escrow_data.xval,
-            yval: escrow_data.yval,
-            a_pub_key: escrow_data.a_pub_key,
-            b_pub_key: escrow_data.b_pub_key,
-            mint_x_pub_key: escrow_data.mint_x_pub_key,
-            mint_y_pub_key: escrow_data.mint_y_pub_key,
-            vault_x_pub_key: escrow_data.vault_x_pub_key,
-            vault_y_pub_key: escrow_data.vault_y_pub_key,
-            init_deposit_status: escrow_data.init_deposit_status, //0: not initialized, 1: initialized but no one deposited, 2: alice deposited, 3:both deposited
-            is_a_withdrawed: if taker_info.key.as_ref() == escrow_data.a_pub_key.as_ref() {
-                1
-            } else {
-                escrow_data.is_a_withdrawed
-            },
-            is_b_withdrawed: if taker_info.key.as_ref() == escrow_data.b_pub_key.as_ref() {
-                1
-            } else {
-                escrow_data.is_b_withdrawed
-            },
-        };
-        msg!("process_withdrawal 7");
-        escrow_data.serialize(&mut &mut escrow_info.data.borrow_mut()[..])?;
-        msg!("process_withdrawal 8");
-        //todo: delete escrow if compeletely done!
-
-        Ok(())
-    }
-
-
-
-
-
-
-    pub fn process_deposit(
-        accounts: &[AccountInfo],
-        pass: u64) -> ProgramResult {
-        let account_info_iter = &mut accounts.iter();
-        let escrow_info = next_account_info(account_info_iter)?; // mint  public address
-        let payer_token_info = next_account_info(account_info_iter)?;
-        let vault_info = next_account_info(account_info_iter)?; // mint  public address
-        let payer_info = next_account_info(account_info_iter)?; // payer_account, is it both public and private key? yeah
-        let token_program_info = next_account_info(account_info_iter)?; // token_program_id
-
-        // Todo: Check if token_program_info is the "real" token program info
-        msg!("process_deposit started here!");
-        let escrow_size:u64 = (mem::size_of::<EscrowData>()).try_into().unwrap();
-        msg!("process_deposit 1: {:?}", escrow_size);
-        let escrow_data = EscrowData::try_from_slice(&escrow_info.data.borrow())?;
-        msg!("process_deposit 1");
-        let amount;
-        msg!("process_deposit 2");
-        if escrow_data.init_deposit_status == 0 {
-            if payer_info.key.as_ref() != escrow_data.a_pub_key.as_ref() {
-                msg!("Account has not initialized yet!");
-                return Err(ProgramError::InvalidAccountData);
-            }
-        }
-        msg!("process_deposit 3");
-        if escrow_data.init_deposit_status == 1 {
-            if payer_info.key.as_ref() != escrow_data.a_pub_key.as_ref() {
-                msg!(
-                    "Initiator ({}) needs to transfer! But this id want's to pay: {}",
-                    escrow_data.a_pub_key,
-                    payer_info.key
-                );
-                return Err(ProgramError::InvalidAccountData);
-            }
-            amount = escrow_data.xval;
-        }else if escrow_data.init_deposit_status == 2 {
-            if payer_info.key.as_ref() != escrow_data.b_pub_key.as_ref() {
-                msg!("other side should transfer at this moment!");
-                return Err(ProgramError::InvalidAccountData);
-            }
-            amount = escrow_data.yval;
-        } else {
-            msg!("Hey it is not the time!");
-            return Err(ProgramError::InvalidAccountData);
-        }
-        msg!("process_deposit 4");
-        deposit_token(
-            payer_info,
-            vault_info,
-            payer_token_info,
-            token_program_info,
-            amount,
-        )?;
-        msg!("process_deposit 5");
-        let escrow_data = EscrowData {
-            xval: escrow_data.xval,
-            yval: escrow_data.yval,
-            a_pub_key: escrow_data.a_pub_key,
-            b_pub_key: escrow_data.b_pub_key,
-            mint_x_pub_key: escrow_data.mint_x_pub_key,
-            mint_y_pub_key: escrow_data.mint_y_pub_key,
-            vault_x_pub_key: escrow_data.vault_x_pub_key,
-            vault_y_pub_key: escrow_data.vault_y_pub_key,
-            init_deposit_status: escrow_data.init_deposit_status + 1, //0: not initialized, 1: initialized but no one deposited, 2: alice deposited, 3:both deposited
-            is_a_withdrawed: 0,
-            is_b_withdrawed: 0,
-        };
-        msg!("process_deposit 6");
-        escrow_data.serialize(&mut &mut escrow_info.data.borrow_mut()[..])?;
-        Ok(())
-    }
-
     pub fn process_init_escrow(
         accounts: &[AccountInfo],
         amount_x: u64,
@@ -224,9 +59,10 @@ impl Processor {
         let rent_info = next_account_info(account_info_iter)?; // solana.py from solana.sysvar import SYSVAR_RENT_PUBKEY
         let system_program_info = next_account_info(account_info_iter)?; // system program public key? public_key(1)?
 
-        msg!("password {}", pass);
-        msg!("password {:?}", pass.to_le_bytes().as_ref());
-        msg!("alice pub key {:?}", alice_info.key.as_ref());
+
+        // msg!("password {}", pass);
+        // msg!("password {:?}", pass.to_le_bytes().as_ref());
+        // msg!("alice pub key {:?}", alice_info.key.as_ref());
         // return Err(ProgramError::InvalidAccountData);
         msg!("init: 1");
         create_account(
@@ -287,6 +123,7 @@ impl Processor {
             ],
             218, // escrow_size
         )?;
+
         msg!("init: 4");
         let escrow_data = EscrowData {
             xval: amount_x,
@@ -306,82 +143,212 @@ impl Processor {
         msg!("init: 6");
         Ok(())
     }
-}
 
-fn deposit_token<'a>(
-    sender_info: &AccountInfo<'a>, //initiator
-    vault_info: &AccountInfo<'a>,
-    token_info: &AccountInfo<'a>,
-    token_program_info: &AccountInfo<'a>,
-    amount: u64,
-) -> ProgramResult {
-    msg!("deposit_token 1");
-    let transfer_from_sender = spl_token::instruction::transfer(
-        token_program_info.key,
-        token_info.key,
-        &vault_info.key,
-        &sender_info.key,    //alice
-        &[&sender_info.key], //alice
-        amount,
-    )?;
-    msg!("deposit_token 2");
-    solana_program::program::invoke(
-        &transfer_from_sender,
-        &[
-            token_info.clone(),  //X_a
-            sender_info.clone(), //alice
-            vault_info.clone(),  //vault_X
-            token_program_info.clone(),
-        ],
-    )?;
-    msg!("deposit_token 3");
 
-    Ok(())
-}
 
-fn withdraw_token<'a>(
-    program_id: &Pubkey,
-    escrow_info: &AccountInfo<'a>,
-    vault_info: &AccountInfo<'a>,
-    token_info: &AccountInfo<'a>,
-    token_program_info: &AccountInfo<'a>,
-    escrow_seed: &[&[u8]],
-    amount: u64,
-) -> ProgramResult {
-    msg!("withdraw_token 1");
-    let (_, escrow_bump_seed) = Pubkey::find_program_address(escrow_seed, program_id);
-    msg!("withdraw_token 2");
-    let esc_seed = &[
-        escrow_seed[0],
-        escrow_seed[1],
-        escrow_seed[2],
-        escrow_seed[3],
-        escrow_seed[4],
-        escrow_seed[5],
-        &[escrow_bump_seed],
-    ];
-    msg!("withdraw_token 3");
-    let transfer_from_vault = spl_token::instruction::transfer(
-        token_program_info.key,
-        &vault_info.key,
-        token_info.key,
-        &escrow_info.key,
-        &[&escrow_info.key],
-        amount,
-    )?;
-    msg!("withdraw_token 4");
-    solana_program::program::invoke_signed(
-        &transfer_from_vault,
-        &[
-            vault_info.clone(),
-            escrow_info.clone(),
-            token_info.clone(),
-            token_program_info.clone(),
-        ],
-        &[esc_seed],
-    )?;
 
-    Ok(())
+    pub fn process_deposit(
+        accounts: &[AccountInfo],
+        pass: u64) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let escrow_info = next_account_info(account_info_iter)?; // mint  public address
+        let payer_token_info = next_account_info(account_info_iter)?;
+        let vault_info = next_account_info(account_info_iter)?; // mint  public address
+        let payer_info = next_account_info(account_info_iter)?; // payer_account, is it both public and private key? yeah
+        let token_program_info = next_account_info(account_info_iter)?; // token_program_id
+
+        // Todo: Check if token_program_info is the "real" token program info
+        msg!("process_deposit started here!");
+        let escrow_size:u64 = (mem::size_of::<EscrowData>()).try_into().unwrap();
+        msg!("process_deposit 1: {:?}", escrow_size);
+        let escrow_data = EscrowData::try_from_slice(&escrow_info.data.borrow())?;
+        msg!("process_deposit 1");
+        let amount;
+        msg!("process_deposit 2");
+        if escrow_data.init_deposit_status == 0 {
+            if payer_info.key.as_ref() != escrow_data.a_pub_key.as_ref() {
+                msg!("Account has not initialized yet!");
+                return Err(ProgramError::InvalidAccountData);
+            }
+        }
+        msg!("process_deposit 3");
+        if escrow_data.init_deposit_status == 1 {
+            if payer_info.key.as_ref() != escrow_data.a_pub_key.as_ref() {
+                msg!(
+                    "Initiator ({}) needs to transfer! But this id want's to pay: {}",
+                    escrow_data.a_pub_key,
+                    payer_info.key
+                );
+                return Err(ProgramError::InvalidAccountData);
+            }
+            amount = escrow_data.xval;
+        }else if escrow_data.init_deposit_status == 2 {
+            if payer_info.key.as_ref() != escrow_data.b_pub_key.as_ref() {
+                msg!("other side should transfer at this moment!");
+                return Err(ProgramError::InvalidAccountData);
+            }
+            amount = escrow_data.yval;
+        } else {
+            msg!("Hey it is not the time!");
+            return Err(ProgramError::InvalidAccountData);
+        }
+        msg!("process_deposit 4");
+        let transfer_from_sender = spl_token::instruction::transfer(
+            token_program_info.key,
+            payer_token_info.key,
+            &vault_info.key,
+            &payer_info.key,    //alice
+            &[&payer_info.key], //alice
+            amount,
+        )?;
+        msg!("deposit_token 2");
+        solana_program::program::invoke(
+            &transfer_from_sender,
+            &[
+                payer_token_info.clone(),  //X_a
+                payer_info.clone(), //alice
+                vault_info.clone(),  //vault_X
+                token_program_info.clone(),
+            ],
+        )?;
+        msg!("deposit_token 3");
+
+
+        msg!("process_deposit 5");
+        let escrow_data = EscrowData {
+            xval: escrow_data.xval,
+            yval: escrow_data.yval,
+            a_pub_key: escrow_data.a_pub_key,
+            b_pub_key: escrow_data.b_pub_key,
+            mint_x_pub_key: escrow_data.mint_x_pub_key,
+            mint_y_pub_key: escrow_data.mint_y_pub_key,
+            vault_x_pub_key: escrow_data.vault_x_pub_key,
+            vault_y_pub_key: escrow_data.vault_y_pub_key,
+            init_deposit_status: escrow_data.init_deposit_status + 1, //0: not initialized, 1: initialized but no one deposited, 2: alice deposited, 3:both deposited
+            is_a_withdrawed: 0,
+            is_b_withdrawed: 0,
+        };
+        msg!("process_deposit 6");
+        escrow_data.serialize(&mut &mut escrow_info.data.borrow_mut()[..])?;
+        Ok(())
+    }
+
+
+    pub fn process_withdrawal(
+        accounts: &[AccountInfo],
+        pass: u64,
+        program_id: &Pubkey) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let escrow_info = next_account_info(account_info_iter)?; // mint  public address
+        let taker_token_info = next_account_info(account_info_iter)?;
+        let vault_info = next_account_info(account_info_iter)?; // mint  public address
+        let taker_info = next_account_info(account_info_iter)?;
+        let token_program_info = next_account_info(account_info_iter)?; // token_program_id
+        msg!("process_withdrawal 1");
+        let escrow_data = EscrowData::try_from_slice(&escrow_info.data.borrow())?;
+        
+        let amount;
+        msg!("process_withdrawal 2");
+        if escrow_data.init_deposit_status <= 2 {
+            msg!("Hey it is not the time to withdraw, you may go ahead and cancel to get back your token!");
+            return Err(ProgramError::InvalidAccountData);
+        }
+        msg!("process_withdrawal 3");
+        if taker_info.key.as_ref() == escrow_data.a_pub_key.as_ref() {
+            if escrow_data.is_a_withdrawed != 0 {
+                msg!("Already done!");
+                return Err(ProgramError::InvalidAccountData);
+            }
+            amount = escrow_data.yval;
+        } else if taker_info.key.as_ref() == escrow_data.b_pub_key.as_ref() {
+            if escrow_data.is_b_withdrawed != 0 {
+                msg!("Already done!");
+                return Err(ProgramError::InvalidAccountData);
+            }
+            amount = escrow_data.xval;
+        } else{
+            msg!("Not authorized!");
+            return Err(ProgramError::InvalidAccountData);
+        };
+        msg!("process_withdrawal 4");
+        let tmp = pass.to_le_bytes();
+        let escrow_seed = &[
+            b"escrow",
+            tmp.as_ref(),
+            escrow_data.a_pub_key.as_ref(),
+            escrow_data.b_pub_key.as_ref(),
+            escrow_data.mint_x_pub_key.as_ref(),
+            escrow_data.mint_y_pub_key.as_ref(),
+        ];
+
+        msg!("withdraw_token 1");
+        let (_, escrow_bump_seed) = Pubkey::find_program_address(escrow_seed, program_id);
+        msg!("withdraw_token 2");
+        let esc_seed = &[
+            escrow_seed[0],
+            escrow_seed[1],
+            escrow_seed[2],
+            escrow_seed[3],
+            escrow_seed[4],
+            escrow_seed[5],
+            &[escrow_bump_seed],
+        ];
+        msg!("withdraw_token 3");
+        let transfer_from_vault = spl_token::instruction::transfer(
+            token_program_info.key,
+            &vault_info.key,
+            taker_token_info.key,
+            &escrow_info.key,
+            &[&escrow_info.key],
+            amount,
+        )?;
+        msg!("withdraw_token 4");
+        solana_program::program::invoke_signed(
+            &transfer_from_vault,
+            &[
+                vault_info.clone(),
+                escrow_info.clone(),
+                taker_token_info.clone(),
+                token_program_info.clone(),
+            ],
+            &[esc_seed],
+        )?;
+        
+
+
+
+
+        msg!("process_withdrawal 6");
+        let escrow_data = EscrowData {
+            xval: escrow_data.xval,
+            yval: escrow_data.yval,
+            a_pub_key: escrow_data.a_pub_key,
+            b_pub_key: escrow_data.b_pub_key,
+            mint_x_pub_key: escrow_data.mint_x_pub_key,
+            mint_y_pub_key: escrow_data.mint_y_pub_key,
+            vault_x_pub_key: escrow_data.vault_x_pub_key,
+            vault_y_pub_key: escrow_data.vault_y_pub_key,
+            init_deposit_status: escrow_data.init_deposit_status, //0: not initialized, 1: initialized but no one deposited, 2: alice deposited, 3:both deposited
+            is_a_withdrawed: if taker_info.key.as_ref() == escrow_data.a_pub_key.as_ref() {
+                1
+            } else {
+                escrow_data.is_a_withdrawed
+            },
+            is_b_withdrawed: if taker_info.key.as_ref() == escrow_data.b_pub_key.as_ref() {
+                1
+            } else {
+                escrow_data.is_b_withdrawed
+            },
+        };
+        msg!("process_withdrawal 7");
+        escrow_data.serialize(&mut &mut escrow_info.data.borrow_mut()[..])?;
+        msg!("process_withdrawal 8");
+        //todo: delete escrow if compeletely done!
+
+        Ok(())
+    }
+    
+    
 }
 
 fn create_account<'a>(
@@ -456,22 +423,23 @@ fn create_escrow_account<'a>(
     payer_info: &AccountInfo<'a>,
     rent_info: &AccountInfo<'a>,
     system_program_info: &AccountInfo<'a>,
-    vault_seed: &[&[u8]],
+    escrow_seed: &[&[u8]],
     space: u64,
 ) -> ProgramResult {
     let rent = &Rent::from_account_info(rent_info)?;
+
     let required_lamports = rent
         .minimum_balance(space.try_into().unwrap())
         .max(1)
         .saturating_sub(escrow_info.lamports());
-    let (_, bump_seed) = Pubkey::find_program_address(vault_seed, program_id);
+    let (_, bump_seed) = Pubkey::find_program_address(escrow_seed, program_id);
     let seed = &[
-        vault_seed[0],
-        vault_seed[1],
-        vault_seed[2],
-        vault_seed[3],
-        vault_seed[4],
-        vault_seed[5],
+        escrow_seed[0],
+        escrow_seed[1],
+        escrow_seed[2],
+        escrow_seed[3],
+        escrow_seed[4],
+        escrow_seed[5],
         &[bump_seed],
     ];
     solana_program::program::invoke_signed(
