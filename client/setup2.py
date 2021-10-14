0cf7b9db2400f84b76ca9a34b69cd1939cc1a24d
tsarkar23 <tuhin91@gmail.com>
2021-10-14 14:53:43 -0500

    setup

--- client/setup2.py
+++ client/setup2.py
@@ -5,6 +5,7 @@ import time
 import argparse
 import sys
 import glob
+import base64
 from solana.rpc.api import Client
 from solana.blockhash import Blockhash
 from solana.publickey import PublicKey
@@ -24,6 +25,7 @@ def _encode(s, pad=32):
     return r
 
 TOKEN_PROGRAM_ID: PublicKey = PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
+# Payer is always Alice
 
 def process_init(program_id, escrow_address, x_mint_pubkey,\
                  y_mint_pubkey, vaultx, vaulty, payer_public_key,\
@@ -98,91 +100,109 @@ def process(args):
     op_type = args.op_type
     user = args.user
     http_client = Client(args.http)
+    program_id =  PublicKey(args.pid)
 
-    # Payer details
-    if args.payerKey:
-        payer_loaded_account = solana.keypair.Keypair(json.load(open(args.payerKey))[:32])
-    elif op_type=='init':
-        print('Need a payer; generating new')
-        payer_loaded_account = solana.keypair.Keypair.generate()
-        tmp = [int(x) for x in payer_loaded_account.secret_key]
-        with open('./payer_secret_key.txt', 'w') as w:
-            json.dump(tmp, w)
-        print('Air drop solana for init; not on mainnet')
-        http_client.request_airdrop(payer_loaded_account.public_key, 1000000000)
-        time.sleep(15)
-    else:
-        payer_loaded_account = None
-        payer_public_key = None
-        print('Payer details not needed')
+    # Seed+password for sending data
+    password = _encode(str(args.password))
 
-    # Alice details    
-    if args.aliceKey:
-        alice_keypair = solana.keypair.Keypair(json.load(open(args.aliceKey))[:32])
-    elif op_type=='init':
-        print('Need Alice; generating new')
-        alice_keypair = solana.keypair.Keypair.generate()
-        tmp = [int(x) for x in alice_keypair.secret_key]
-        with open('./alice_secret_key.txt', 'w') as w:
-            json.dump(tmp, w)
-        with open('./alice_public_key.txt', 'w') as w:
-            json.dump(str(alice_keypair.public_key), w)
-        print('Air drop solana for init; not on mainnet')
-        http_client.request_airdrop(alice_keypair.public_key, 1000000000)
-        time.sleep(15)
-    elif user=='alice':
-        print('Alice details needed')
-        return
-    else:
-        print('Nothing to do for Alice')
-        alice_keypair = None
-        if args.apubk is None:
-            print('Need Alice public key!!!')
+
+    if op_type != 'init': 
+        escrow_address = args.escrow
+        token_acc = args.token
+        user_keypair = solana.keypair.Keypair(json.load(open(args.userKP))[:32])
+
+        if escrow_address is None:
+            print('This is not init, and is required')
             return
-        alice_pubkey = PublicKey(json.load(open(args.apubk)))
-
-    # Bob details
-    if args.bobKey: 
-        bob_keypair = solana.keypair.Keypair(json.load(open(args.bobKey))[:32])
-    elif op_type=='init':
-        print('Need Bob; generating new')
-        bob_keypair = solana.keypair.Keypair.generate()
-        tmp = [int(x) for x in bob_keypair.secret_key]
-        with open('./bob_secret_key.txt', 'w') as w:
-            json.dump(tmp, w)
-        with open('./bob_public_key.txt', 'w') as w:
-            json.dump(str(bob_keypair.public_key), w)
-        print('Air drop solana for init; not on mainnet')
-        http_client.request_airdrop(bob_keypair.public_key, 1000000000)
-        time.sleep(15)
-    elif user=='bob':
-        print('Bob details needed')
-        return
-    else:
-        print('Nothing to do for Bob')
-        bob_keypair = None
-        if args.bpubk is None:
-            print('Need Bob public key!!!')
+        if token_acc is None:
+            print('No token account; Required')
             return
-        bob_pubkey = PublicKey(json.load(open(args.bpubk)))
-    
-    # Program details
-    program_id = PublicKey(args.pid)
 
-    if alice_keypair is not None:
-        alice_pubkey = alice_keypair.public_key
-    if bob_keypair is not None:
-        bob_pubkey = bob_keypair.public_key
-    if payer_loaded_account is not None:
+        escrow_address = PublicKey(escrow_address)
+        token_account = PublicKey(token_acc)
+
+        info_data = base64.b64decode(http_client.get_account_info(escrow_address)['result']['value']['data'][0])
+        # Do NOT mess with this
+        alice_pubkey = PublicKey(info_data[16:48])
+        bob_pubkey = PublicKey(info_data[48:80])
+        x_mint_pubkey = PublicKey(info_data[80:112])
+        y_mint_pubkey = PublicKey(info_data[112:144])
+
+        x_seeds = [
+            bytes(str(args.xpass), encoding='utf8'),
+            bytes(alice_pubkey),
+            bytes(bob_pubkey),
+            bytes(x_mint_pubkey),
+            bytes(y_mint_pubkey),
+            password,
+        ]
+
+        y_seeds = [
+            bytes(str(args.ypass), encoding='utf8'),
+            bytes(alice_pubkey),
+            bytes(bob_pubkey),
+            bytes(x_mint_pubkey),
+            bytes(y_mint_pubkey),
+            password,
+        ]
+
+        escrow_seeds = [
+            bytes(str(args.epass), encoding='utf8'),
+            bytes(alice_pubkey),
+            bytes(bob_pubkey),
+            bytes(x_mint_pubkey),
+            bytes(y_mint_pubkey),
+            password,
+        ]
+        vaultx, xseed = PublicKey.find_program_address(seeds=x_seeds,program_id=program_id)
+        vaulty, yseed = PublicKey.find_program_address(seeds=y_seeds,program_id=program_id)
+        escrow_address, escrow_seed = PublicKey.find_program_address(seeds=escrow_seeds,program_id=program_id)
+
+        if op_type=='deposit':
+            if user=='alice':
+                
+                # Alice deposit X
+                print('Alice is depositing X')
+                data = pack('<B', 1)+password
+                process_deposit(program_id, escrow_address, token_account, vaultx,\
+                                alice_pubkey, data, http_client, user_keypair)
+            elif user=='bob':
+                print('Bob is depositing Y')
+                # Bob deposit Y
+                data = pack('<B', 1)+password
+                process_deposit(program_id, escrow_address, token_account, vaulty,\
+                                bob_pubkey, data, http_client, user_keypair)
+
+        elif op_type=='withdraw':
+            if user=='alice':
+                
+                # Alice withdraw Y
+                print('Alice is withdrawing Y')
+                data = pack('<B', 2)+password
+                process_deposit(program_id, escrow_address, token_account, vaulty,\
+                                alice_pubkey, data, http_client, user_keypair)
+            elif user=='bob':
+                print('Bob is withdrawing X')
+                # Bob withdraw X
+                data = pack('<B', 2)+password
+                process_deposit(program_id, escrow_address, token_account, vaultx,\
+                                bob_pubkey, data, http_client, user_keypair)
+
+    else:
+        if args.user=='bob':
+            print('Only Alice can initialize')
+            return
+        payer_loaded_account = solana.keypair.Keypair(json.load(open(args.payerKP))[:32])
         payer_public_key = payer_loaded_account.public_key
+        alice_pubkey = PublicKey(args.apubk)
+        bob_pubkey = PublicKey(args.bpubk)
 
-    print(f"payer Account: {payer_public_key}")
-    print(f"alice Account: {alice_pubkey}")
-    print(f"bob Account: {bob_pubkey}")
-   
+        
+        print(f"payer Account: {payer_public_key}")
+        print(f"alice Account: {alice_pubkey}")
+        print(f"bob Account: {bob_pubkey}")
 
-    # Always create token accounts
-    if op_type=='init':
+        # Creating Token Accounts
         X_mint_account_address = Token.create_mint(conn= http_client, payer = payer_loaded_account,\
                                                 mint_authority = payer_loaded_account.public_key,\
                                                 decimals = 0, program_id = TOKEN_PROGRAM_ID, )
@@ -194,9 +214,9 @@ def process(args):
         bob_X_token_account = X_mint_account_address.create_associated_token_account(bob_pubkey)
         bob_Y_token_account = Y_mint_account_address.create_associated_token_account(bob_pubkey)
 
-        X_mint_account_address.mint_to(alice_X_token_account,payer_loaded_account,args.xtoken)
-        Y_mint_account_address.mint_to(bob_Y_token_account,payer_loaded_account,args.ytoken)
-
+        X_mint_account_address.mint_to(alice_X_token_account,payer_loaded_account,args.numx)
+        Y_mint_account_address.mint_to(bob_Y_token_account,payer_loaded_account,args.numy)
+    
         # Writing all mint details
         with open('./mint_x.txt', 'w') as w:
             json.dump(str(X_mint_account_address.pubkey), w)
@@ -209,60 +229,46 @@ def process(args):
         with open('./bob_x_token.txt', 'w') as w:
             json.dump(str(bob_X_token_account), w)
         with open('./bob_y_token.txt', 'w') as w:
-            json.dump(str(bob_Y_token_account), w)   
-           
+            json.dump(str(bob_Y_token_account), w)  
+        
         x_mint_pubkey = X_mint_account_address.pubkey
         y_mint_pubkey = Y_mint_account_address.pubkey
 
-    elif user=='alice':
-        x_mint_pubkey = PublicKey(json.load(open(args.mintx)))
-        y_mint_pubkey = PublicKey(json.load(open(args.minty)))
-        alice_X_token_account = PublicKey(json.load(open(args.aTokenX)))        
-        alice_Y_token_account = PublicKey(json.load(open(args.aTokenY)))
-    elif user=='bob':
-        x_mint_pubkey = PublicKey(json.load(open(args.mintx)))
-        y_mint_pubkey = PublicKey(json.load(open(args.minty)))
-        bob_X_token_account = PublicKey(json.load(open(args.bTokenX)))
-        bob_Y_token_account = PublicKey(json.load(open(args.bTokenY)))
+        x_seeds = [
+            bytes(str(args.xpass), encoding='utf8'),
+            bytes(alice_pubkey),
+            bytes(bob_pubkey),
+            bytes(x_mint_pubkey),
+            bytes(y_mint_pubkey),
+            password,
+        ]
 
+        y_seeds = [
+            bytes(str(args.ypass), encoding='utf8'),
+            bytes(alice_pubkey),
+            bytes(bob_pubkey),
+            bytes(x_mint_pubkey),
+            bytes(y_mint_pubkey),
+            password,
+        ]
 
-    # Seed+password for sending data
-    password = _encode(str(args.password))
+        escrow_seeds = [
+            bytes(str(args.epass), encoding='utf8'),
+            bytes(alice_pubkey),
+            bytes(bob_pubkey),
+            bytes(x_mint_pubkey),
+            bytes(y_mint_pubkey),
+            password,
+        ]
 
-    x_seeds = [
-        bytes(str(args.xpass), encoding='utf8'),
-        bytes(alice_pubkey),
-        bytes(bob_pubkey),
-        bytes(x_mint_pubkey),
-        bytes(y_mint_pubkey),
-        password,
-    ]
+        # create vaults, necessary?
+        vaultx, xseed = PublicKey.find_program_address(seeds=x_seeds,program_id=program_id)
+        vaulty, yseed = PublicKey.find_program_address(seeds=y_seeds,program_id=program_id)
+        escrow_address, escrow_seed = PublicKey.find_program_address(seeds=escrow_seeds,program_id=program_id)
+        print(f'Escrow is here {escrow_address}')
+        with open('./escrow_account.txt', 'w') as w:
+            json.dump(str(escrow_address), w)  
 
-    y_seeds = [
-        bytes(str(args.ypass), encoding='utf8'),
-        bytes(alice_pubkey),
-        bytes(bob_pubkey),
-        bytes(x_mint_pubkey),
-        bytes(y_mint_pubkey),
-        password,
-    ]
-
-    escrow_seeds = [
-        bytes(str(args.epass), encoding='utf8'),
-        bytes(alice_pubkey),
-        bytes(bob_pubkey),
-        bytes(x_mint_pubkey),
-        bytes(y_mint_pubkey),
-        password,
-    ]
-
-    # create vaults, necessary?
-    vaultx, xseed = PublicKey.find_program_address(seeds=x_seeds,program_id=program_id)
-    vaulty, yseed = PublicKey.find_program_address(seeds=y_seeds,program_id=program_id)
-    escrow_address, escrow_seed = PublicKey.find_program_address(seeds=escrow_seeds,program_id=program_id)
-
-
-    if op_type == 'init': 
         print('Start Initializing Accounts')
         # initialize transaction
         x_val = args.xtoken
@@ -272,65 +278,30 @@ def process(args):
                      y_mint_pubkey, vaultx, vaulty, payer_public_key,\
                      alice_pubkey, bob_pubkey, data, http_client, payer_loaded_account)
     
-    elif op_type=='deposit':
-        if user=='alice':
-            
-            # Alice deposit X
-            print('Alice is depositing X')
-            data = pack('<B', 1)+password
-            process_deposit(program_id, escrow_address, alice_X_token_account, vaultx,\
-                            alice_pubkey, data, http_client, alice_keypair)
-        elif user=='bob':
-            print('Bob is depositing Y')
-            # Bob deposit Y
-            data = pack('<B', 1)+password
-            process_deposit(program_id, escrow_address, bob_Y_token_account, vaulty,\
-                            bob_pubkey, data, http_client, bob_keypair)
-
-    elif op_type=='withdraw':
-        if user=='alice':
-            
-            # Alice withdraw Y
-            print('Alice is withdrawing Y')
-            data = pack('<B', 2)+password
-            process_deposit(program_id, escrow_address, alice_Y_token_account, vaulty,\
-                            alice_pubkey, data, http_client, alice_keypair)
-        elif user=='bob':
-            print('Bob is withdrawing X')
-            # Bob withdraw X
-            data = pack('<B', 2)+password
-            process_deposit(program_id, escrow_address, bob_X_token_account, vaultx,\
-                            bob_pubkey, data, http_client, bob_keypair)
-
 def get_parser():
     """
     Creates a new argument parser.
     """
     parser = argparse.ArgumentParser(description='Send Escrow Data')
 
-    parser.add_argument('--aliceKey', '-a', help='alice private key file', default=None)
-    parser.add_argument('--bobKey', '-b', help='bob private key file', default=None)
-    parser.add_argument('--apubk', help='alice public key file', default=None)
-    parser.add_argument('--bpubk', help='bob public key file', default=None)
-    parser.add_argument('--payerKey', '-p', help='payer private key: must have sols', default=None)
+    parser.add_argument('--payerKP', '-p', help='Payer Keypair file', default=None)
     parser.add_argument('--http', help='http client', default='https://api.devnet.solana.com')
+    parser.add_argument('--op_type', help='Operation Type', default='init', choices=['init', 'deposit', 'withdraw'])
+    parser.add_argument('--user', help='User', default='alice', choices=['alice', 'bob'])
+    parser.add_argument('--userKP', help='User Keypair file', default=None)
+    parser.add_argument('--token', help='User Token Public Key', default=None)
+    parser.add_argument('--escrow', help='Escrow Address', default=None)
     parser.add_argument('--password', '-X', help='Password', default='password')
-    parser.add_argument('--numx', help='Min X tokens', default=1000)
-    parser.add_argument('--numy', help='Min Y tokens', default=100)
     parser.add_argument('--pid', help='Program ID', required=True)
+    parser.add_argument('--apubk', help='Alice Public Key', default=None)
+    parser.add_argument('--bpubk', help='Bob Public Key', default=None)
     parser.add_argument('--xpass', help='Vault X Pass', default="vault_x")
     parser.add_argument('--ypass', help='Vault Y Pass', default="vault_y")
     parser.add_argument('--epass', help='Escrow Pass', default="escrow")
     parser.add_argument('--xtoken', help='Send #X tokens', default=15)
     parser.add_argument('--ytoken', help='Send #Y tokens', default=13)
-    parser.add_argument('--aTokenX', help='Alice X Token Account File', default=None)
-    parser.add_argument('--aTokenY', help='Alice Y Token Account File', default=None)
-    parser.add_argument('--bTokenX', help='Bob X Token Account File', default=None)
-    parser.add_argument('--bTokenY', help='Bob Y Token Account File', default=None)
-    parser.add_argument('--mintx', help='X Mint Account Public Key File', default=None)
-    parser.add_argument('--minty', help='Y Mint Account Public Key File', default=None)
-    parser.add_argument('--op_type', help='Operation Type', default='init', choices=['init', 'deposit', 'withdraw'])
-    parser.add_argument('--user', help='Alice/Bob', default='alice', choices=['alice', 'bob'])
+    parser.add_argument('--numx', help='Min X tokens', default=1000)
+    parser.add_argument('--numy', help='Min Y tokens', default=100)
 
     return parser
 
